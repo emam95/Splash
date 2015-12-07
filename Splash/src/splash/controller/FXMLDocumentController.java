ac754410e43fcc58e12f78fe72c66fbc8fc722d4
emam <m.emam9595@gmail.com>
2015-12-07 19:25:48 +0200

    MVC startup

--- Splash/src/splash/controller/FXMLDocumentController.java
+++ Splash/src/splash/controller/FXMLDocumentController.java
@@ -3,13 +3,17 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package splash;
+package splash.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.ToolBar;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 
 /**
  * FXML Controller class
@@ -20,6 +24,18 @@ public class FXMLDocumentController implements Initializable {
 
     @FXML
     private Pane DrawingPanel;
+    @FXML
+    private ToolBar tools1;
+    @FXML
+    private ToolBar tools2;
+    @FXML
+    private VBox LayersPanel;
+    @FXML
+    private Button addLayerBtn;
+    @FXML
+    private Button delLayerBtn;
+    @FXML
+    private ColorPicker colorPicker;
 
     /**
      * Initializes the controller class.
