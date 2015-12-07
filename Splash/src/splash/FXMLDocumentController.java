78d5b9a5b81bc586008f1e54adb716c8607adcae
emam <m.emam9595@gmail.com>
2015-12-07 18:37:05 +0200

    Gui prototype

--- Splash/src/splash/FXMLDocumentController.java
+++ Splash/src/splash/FXMLDocumentController.java
@@ -12,20 +12,16 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 
-/**
- *
- * @author Hesham
- */
+
 public class FXMLDocumentController implements Initializable {
-    
+
     @FXML
-    private Label label;
-    
-    @FXML
+    private Pane DrawingPanel;
+        
     private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+        
     }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
